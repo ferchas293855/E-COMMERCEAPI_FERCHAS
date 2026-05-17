fe8a969daf580601a26eb5e612ec142180561a23
ferchas293855 <ferchas2015@gmail.com>
2026-05-16 23:49:23 -0600

    Implementar acordeones para productos, carritos y login en la interfaz, y agregar funcionalidades de búsqueda y autenticación de usuarios.

--- scripts/script_e-commerce.js
+++ scripts/script_e-commerce.js
@@ -4,21 +4,36 @@ const buscarInput = document.getElementById("buscarInput");
 const getProductos = document.getElementById("obtener");
 const getP = document.getElementById("buscar_productos");
 const contenido = document.getElementById("resultado");
-const carritoTodo = document.getElementById("todo_carrito");
 const carritoBuscar = document.getElementById("buscar_carrito");
 const guardar = document.getElementById("btnGuardar");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const login = document.getElementById("login");
+const usuario = document.getElementById("buscar_usuarios");
+const btnUusuario = document.getElementById("obtener_usuarios");
 
 const inicializar = () => {
   //funciones por eventos de delegacion
   getProductos.addEventListener("click", obtener);
   getP.addEventListener("click", obtenerP);
-  carritoTodo.addEventListener("click", obtenerC);
   carritoBuscar.addEventListener("click", obtenerCarritoUsuario);
+  login.addEventListener("click", IniciarSesion);
+  btnUusuario.addEventListener("click", listaUsuarios);
 };
 const obtener = async () => {
+  if (!input.value) {
+    alert("ingrese un valor para obtener los productos");
+    return;
+  }
   try {
     const data = await fetch("https://fakestoreapi.com/products");
     const res = await data.json();
+    if (input.value > res.length) {
+      alert(
+        `el numero ingresado es mayor a la cantidad de productos disponibles, ingrese un numero menor a ${res.length}`,
+      );
+      return;
+    }
     console.log(res);
     // destructuracion de lo que regrese la api
     // const {id,title,price, descripcion,category, image} = res;
@@ -43,6 +58,10 @@ const obtener = async () => {
   }
 };
 const obtenerP = async () => {
+  if (!buscarInput.value) {
+    alert("ingrese un valor para encontrar el producto");
+    return;
+  }
   try {
     const data = await fetch(
       `https://fakestoreapi.com/products/${buscarInput.value}`,
@@ -64,16 +83,6 @@ const obtenerP = async () => {
     alert("fallo al hacer la peticion");
   }
 };
-const obtenerC = async () => {
-  try {
-    const res = await fetch("https://fakestoreapi.com/carts");
-    const data = await res.json();
-    console.log(data);
-  } catch (error) {
-    alert("hubo un error al obtener el carrito", error);
-    return;
-  }
-};
 const obtenerCarritoUsuario = async () => {
   const inputC = document.getElementById("buscarCarrito");
   if (!inputC.value) {
@@ -85,6 +94,10 @@ const obtenerCarritoUsuario = async () => {
     const data = await res.json();
     console.log(data);
     const filtro = data.find((nombre) => inputC.value == nombre.userId);
+    if (!filtro) {
+      alert("no se encontro el carrito del usuario ingresado");
+      return;
+    }
     console.log(filtro);
     const productosFiltrados = await Promise.all(
       filtro.products.map(async (p) => {
@@ -123,11 +136,11 @@ function renderizarProductos(id, userId, productoR) {
               (p) => `
               <div class="producto-item">
                   <img src="${p.image}" alt="${p.title}" width="50">
-                  <h5 class="p-titulo">${p.title}</h5>
+                  <h5 class="p-titulo">TITULO: ${p.title}</h5>
                   <p class="p-precio">Precio unitario: $${p.price}</p>
                   <p>Cantidad: ${p.quantity}</p>
-                  <p class="p-desc">Descripcion: ${p.description}</p>
-                  <p class="p-cat">Categoria: ${p.category}</p>
+                  <p class="p-desc">DESCRIPCION: ${p.description}</p>
+                  <p class="p-cat">CATEGORIA: ${p.category}</p>
                   <p>Cantidad: ${p.quantity}</p>
                   <p><strong>Subtotal: $${(p.price * p.quantity).toFixed(2)}</strong></p>
                   <button onclick="editar(${p.id},this)">Editar</button>
@@ -186,10 +199,14 @@ const editar = async (id, btn) => {
           const data = await res.json();
           const tarjeta = btn.closest(".tarjeta");
           if (tarjeta) {
-            tarjeta.querySelector(".p-titulo").textContent = data.title;
-            tarjeta.querySelector(".p-desc").textContent = data.description;
-            tarjeta.querySelector(".p-precio").textContent = data.price;
-            tarjeta.querySelector(".p-cat").textContent = data.category;
+            tarjeta.querySelector(".p-titulo").textContent =
+              `TITULO: ${data.title}`;
+            tarjeta.querySelector(".p-desc").textContent =
+              `DESCRIPCION: ${data.description}`;
+            tarjeta.querySelector(".p-precio").textContent =
+              `PRECIO: $${data.price}`;
+            tarjeta.querySelector(".p-cat").textContent =
+              `CATEGORIA: ${data.category}`;
             alert("ACTUALIZADO CORRECTAMENTE!!");
           } else {
             alert("no se encontro la tarjeta");
@@ -208,5 +225,78 @@ const editar = async (id, btn) => {
 };
 const eliminar = async (id, btn) => {
   console.log("el id de la tarjeta es: ", id, "y su referencia es", btn);
+  try {
+    const res = await fetch(`https://fakestoreapi.com/products/${id}`, {
+      method: "DELETE",
+    });
+    const data = await res.json();
+    if (res.ok) {
+      const tarjeta = btn.closest(".tarjeta");
+      setTimeout(() => {
+        tarjeta.style.opacity = "0.3";
+        alert("ELIIMINADO CORRECTAMENTE!!");
+        tarjeta.remove();
+      }, 1500);
+    }
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+const IniciarSesion = async () => {
+  if (!username.value || !password.value) {
+    alert("ingrese un correo o contraseña");
+  }
+  console.log("el user y la contra son: ", username.value, password.value);
+
+  try {
+    const user = username.value;
+    const contra = password.value;
+    const data = await fetch("https://fakestoreapi.com/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: user,
+        password: contra,
+      }),
+    });
+    const res = await data.json();
+    console.log(res);
+    alert("SE INICIO CORRECTAMENTE SESION, BIENVENIDO");
+  } catch (error) {
+    alert("no se pudo iniciar sesion", error);
+    console.error(error);
+  }
+};
+const listaUsuarios = async () => {
+  try {
+    const data = await fetch("https://fakestoreapi.com/users");
+    const res = await data.json();
+    console.log(res);
+    console.log(usuario.value);
+    if (usuario.value > res.length) {
+      alert(
+        `el numero ingresado es mayor a la cantidad de usuarios disponibles, ingrese un numero menor a ${res.length}`,
+      );
+      return;
+    }
+
+    // destructuracion de lo que regrese la api
+    // const {id,title,price, descripcion,category, image} = res;
+    contenido.innerHTML = res
+      .slice(0, usuario.value)
+      .map(
+        (user) =>
+          `<div class="tarjeta">
+            <h5>ID: ${user.id}</h5>
+            <h5 class="p-titulo">USERNAME ${user.username}</h5>
+            <h5 class="p-precio">EMAIL: ${user.email}</h5>
+            <p class="p-desc">PASSWORD: ${user.password}</p>
+        </div>`,
+      )
+      .join("");
+  } catch (error) {
+    alert("fallo al hacer la peticion");
+  }
 };
 inicializar();
